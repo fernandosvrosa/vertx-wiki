e480b7d1e40593e6a80a4bf4bebbfc20fa8c6d47
Fernando da Silva Rosa <fernandorosa@sas.com.br>
2020-06-15 09:10:56 -0300

    first

---
+++ src/main/java/io/vertx/example/MainVerticle.java
@@ -0,0 +1,37 @@
+package io.vertx.example;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Promise;
+
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Promise<Void> promise) throws Exception {
+
+        Promise<String> dbVerticleDeployment = Promise.promise();
+        vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment);
+
+        dbVerticleDeployment.future().compose(id -> {
+
+            Promise<String> httpVerticleDeployment = Promise.promise();
+            vertx.deployVerticle("io.vertx.example.HttpServerVerticle",
+                    new DeploymentOptions().setInstances(2),
+                    httpVerticleDeployment);
+
+            return httpVerticleDeployment.future();
+
+        }).setHandler(ar -> {
+            if (ar.succeeded()) {
+                promise.complete();
+            } else {
+                promise.fail(ar.cause());
+            }
+        });
+    }
+
+
+
+
+}
